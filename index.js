283b6e5d50f5e3610502d12a4710b034e03d7bdc
aman-mayank <133030263+aman-mayank@users.noreply.github.com>
2023-08-24 09:40:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('hello mate...i'm invincible'); //write a response to the client
+  res.write('hello mate...im invincible'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
